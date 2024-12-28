4eec66656c4e380008476917aaf0d76bcbdf35d8
limitify <aldrick175@gmail.com>
2024-12-28 13:07:54 +0200

    Rolled back

--- app/src/pages/dashboard/rfid.jsx
+++ app/src/pages/dashboard/rfid.jsx
@@ -47,7 +47,7 @@ function RFID() {
     // Handle CORS origin logic
   };
   function handleDownloadTemplate() {
-    fetch(`${import.meta.env.APP_URL}/card/download-template`) // Replace with your backend URL
+    fetch(`https://ats-kkcp.onrender.com/api/v1/card/download-template`) // Replace with your backend URL
       .then((response) => {
         if (!response.ok) {
           throw new Error('Failed to download template');

624068b6edb9b4ffeda8629a186c04ca3345878b
limitify <aldrick175@gmail.com>
2024-12-31 12:25:43 +0200

    Corrected backend URL

--- app/src/pages/dashboard/rfid.jsx
+++ app/src/pages/dashboard/rfid.jsx
@@ -47,7 +47,7 @@ function RFID() {
     // Handle CORS origin logic
   };
   function handleDownloadTemplate() {
-    fetch(`https://ats-kkcp.onrender.com/card/download-template`) // Replace with your backend URL
+    fetch(`https://ats-kkcp.onrender.com/api/v1/card/download-template`) // Replace with your backend URL
       .then((response) => {
         if (!response.ok) {
           throw new Error('Failed to download template');
